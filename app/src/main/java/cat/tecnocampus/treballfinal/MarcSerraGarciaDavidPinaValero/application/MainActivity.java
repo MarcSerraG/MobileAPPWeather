415dbe0707e7e0ddd4b656052a1c5abe1ad86286
David Pina <dpina@edu.tecnocampus.cat>
2020-06-19 18:19:37 +0200

    main layout

--- app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/application/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/application/MainActivity.java
@@ -1,8 +1,10 @@
-package cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero;
+package cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.R;
 
 public class MainActivity extends AppCompatActivity {
 
