a0b2a57dbce647aa8fde48d64e608ae48ee56c59
David Pina <dpina@edu.tecnocampus.cat>
2020-06-19 23:30:55 +0200

    Added City and json

---
+++ app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/domain/City.java
@@ -0,0 +1,71 @@
+package cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.domain;
+
+import java.io.Serializable;
+
+public class City implements Serializable {
+
+    private int id;
+    private String name;
+    private String state;
+    private String country;
+    private String longitude;
+    private String latitude;
+
+
+    public City(int id, String name, String state, String country, String longitude, String latitude) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+        this.country = country;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}
