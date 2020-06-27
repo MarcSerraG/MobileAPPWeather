c3c69fa96abd7031c250c8ab0389e7fd893917cf
Marc <mserraga@edu.tecnocampus.cat>
2020-06-27 16:47:58 +0200

    Added recyclerview search view

--- app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/application/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/application/MainActivity.java
@@ -1,8 +1,14 @@
 package cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import androidx.appcompat.widget.SearchView;
 
 import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.R;
 
@@ -12,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.city_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.city_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //adapter
+                //adapter.getFilter().filter(newText)
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
