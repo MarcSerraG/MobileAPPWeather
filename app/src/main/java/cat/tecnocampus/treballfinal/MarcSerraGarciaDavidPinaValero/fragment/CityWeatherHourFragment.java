831d62f94c6cd814c41c67f5a2010e58c2aa6c2f
Marc <mserraga@edu.tecnocampus.cat>
2020-06-30 22:18:35 +0200

    Added more features

--- app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/fragment/CityWeatherHourFragment.java
+++ app/src/main/java/cat/tecnocampus/treballfinal/MarcSerraGarciaDavidPinaValero/fragment/CityWeatherHourFragment.java
@@ -9,7 +9,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.bumptech.glide.Glide;
@@ -17,6 +22,7 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 
 import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.R;
+import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.adapter.CityWeatherHourlyAdapter;
 import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.domain.City;
 import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.domain.CityWeather;
 import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.domain.WeatherReport;
@@ -24,11 +30,14 @@ import cat.tecnocampus.treballfinal.MarcSerraGarciaDavidPinaValero.domain.Weathe
 public class CityWeatherHourFragment extends Fragment {
 
     private WeatherReport weatherReport;
+    private ArrayList<WeatherReport> weatherHourly;
     private City city;
 
     private TextView maxTemperature, minTemperature, currentCityName, currentTemperature;
     private TextView longitud, latitud, humidity;
     private ImageView weatherImage;
+    private RecyclerView viewHourly;
+    private CityWeatherHourlyAdapter cityWeatherHourlyAdapter;
 
     public CityWeatherHourFragment(){}
 
@@ -36,6 +45,7 @@ public class CityWeatherHourFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         this.weatherReport = (WeatherReport) getArguments().getSerializable("current");
+        this.weatherHourly = (ArrayList<WeatherReport>) getArguments().getSerializable("hourly");
         this.city = (City) getArguments().getSerializable("city");
 
         View view = inflater.inflate(R.layout.city_weather_hour_fragment, container, false);
@@ -47,19 +57,29 @@ public class CityWeatherHourFragment extends Fragment {
         latitud = view.findViewById(R.id.tv_current_latitude);
         longitud = view.findViewById(R.id.tv_current_longitude);
         humidity = view.findViewById(R.id.tv_current_humidity);
+        viewHourly = view.findViewById(R.id.rv_city_weather_hourly);
 
         weatherImage = view.findViewById(R.id.im_current_weather);
         //maxTemperature.setText("");
         //minTemperature.setText("");
         currentCityName.setText(""+city.getName());
         currentTemperature.setText(""+(int)weatherReport.getTemperature()+" ºC");
-        latitud.setText(""+city.getLatitude());
-        longitud.setText(""+city.getLongitude());
-        humidity.setText(""+weatherReport.getHumidity());
+        latitud.setText("Lat. "+String.format("%.2f", city.getLatitude()));
+        longitud.setText("Lon. "+String.format("%.2f", city.getLongitude()));
+        humidity.setText(""+weatherReport.getHumidity()+" %");
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this.getContext(),DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(this.getContext(),R.drawable.city_divider));
 
         Glide.with(view.getContext())
                 .load(weatherReport.getIconURL())
                 .into(weatherImage);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        viewHourly.setLayoutManager(layoutManager);
+        cityWeatherHourlyAdapter = new CityWeatherHourlyAdapter(weatherHourly);
+        viewHourly.addItemDecoration(dividerItemDecoration);
+        viewHourly.setAdapter(cityWeatherHourlyAdapter);
 
         return view;
     }
